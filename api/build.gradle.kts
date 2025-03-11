0bc007ce6ff45de702cd9846405353c829acc14b
decryptable <decryptable1337@gmail.com>
2025-03-11 22:41:55 +0000

    Bump version to 1.0.1 for libxposed-api

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -37,7 +37,7 @@ publishing {
         register<MavenPublication>("libxposed-api") {
             artifactId = "libxposed-api"
             group = "io.github.decryptable"
-            version = "1.0.0"
+            version = "1.0.1"
             pom {
                 name.set("libxposed-api")
                 description.set("Modern Xposed API")
